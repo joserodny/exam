0304ac95bdcb59998830ff8de3397c397786d50c
jose rodny sibugan <41980624+joserodny@users.noreply.github.com>
2022-07-11 22:26:25 +0800

    Update statement.sql

--- statement.sql
+++ statement.sql
@@ -1,12 +1,8 @@
-select 
-distinct
-u.email,
-u.name,
-max(case when (m.meta_key='ip_address') then UserColumn2 else NULL end) as 'ip_address',
-max(case when (m.meta_key='referrer') then UserColumn2 else NULL end) as 'referrer',
-max(case when (m.meta_key='user_agent') then UserColumn2 else NULL end) as 'user_agent',
-i.path
-from tbl_user as u 
-RIGHT JOIN tbl_users_meta as m ON m.user_id u.id 
-RIGHT JOIN tbl_users_images as i ON i.user_id i.id 
-GROUP BY u.id
+SELECT u.*,
+case when m.meta_key = "ip_address" then m.meta_value end as ip_address,
+case when m.meta_key = "referrer" then m.meta_value end as referrer,
+case when m.meta_key = "user_agent" then m.meta_value end as user_agent,
+i.url_path
+FROM tbl_users AS u  
+RIGHT JOIN tbl_users_meta AS m ON m.user_id = u.id
+RIGHT JOIN tbl_users_images AS i ON m.user_id = i.id
